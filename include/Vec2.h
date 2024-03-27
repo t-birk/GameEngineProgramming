9151900d7f2b4eeb3643c84af136d47b60270e2b
t-birk <129518957+t-birk@users.noreply.github.com>
2024-03-26 22:11:49 -0400

    Create Vec2.h

---
+++ include/Vec2.h
@@ -0,0 +1,25 @@
+class Vec2
+{
+public:
+
+    float x = 0;
+    float y = 0;
+
+    Vec2();
+    Vec2(float xin, float yin);
+
+    bool operator == (const Vec2& rhs) const;
+    bool operator != (const vec2 & rhs) const;
+
+    Vec2 operator + (const Vec2& rhs) const;
+    Vec2 operator - (const vec2 & rhs) const;
+    Vec2 operator * (const Vec2& rhs) const;
+    Vec2 operator / (const vec2 & rhs) const;
+
+    void operator += (const Vec2& rhs) const;
+    void operator -= (const vec2 & rhs) const;
+    void operator *= (const Vec2& rhs) const;
+    void operator /= (const vec2 & rhs) const;
+
+    float dist(const Vec2 & rhs) const;
+}
