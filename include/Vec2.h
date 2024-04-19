d3edb2b1ddc9d36323bba3f664596171cab469ac
t-birk <129518957+t-birk@users.noreply.github.com>
2024-04-19 09:01:55 -0400

    Update Vec2.h

--- include/Vec2.h
+++ include/Vec2.h
@@ -1,3 +1,5 @@
+#pragma once
+
 class Vec2
 {
 public:
@@ -13,13 +15,13 @@ public:
 
     Vec2 operator + (const Vec2& rhs) const;
     Vec2 operator - (const vec2 & rhs) const;
-    Vec2 operator * (const Vec2& rhs) const;
-    Vec2 operator / (const vec2 & rhs) const;
+    Vec2 operator * (const float val) const;
+    Vec2 operator / (const float val) const;
 
     void operator += (const Vec2& rhs) const;
     void operator -= (const vec2 & rhs) const;
-    void operator *= (const Vec2& rhs) const;
-    void operator /= (const vec2 & rhs) const;
+    void operator *= (const float val) const;
+    void operator /= (const float val) const;
 
     float dist(const Vec2 & rhs) const;
-}
+};
